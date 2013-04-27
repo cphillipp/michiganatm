313b7930d6b6f6939ca15f736b892767b75c23d6
Clayton Phillipp <phillipp@Claytons-MacBook-Pro.local>
2013-04-26 23:07:40 -0600

    updated footer

--- wp-content/themes/the-bootstrap/templates/home.php
+++ wp-content/themes/the-bootstrap/templates/home.php
@@ -10,7 +10,7 @@
 
 get_header(); ?>
 
-<section id="primary" class="span12 home">
+<section id="primary" class="span12">
 	<?php tha_content_before(); ?>
 	<div id="content" role="main">
 		<?php tha_content_top();
